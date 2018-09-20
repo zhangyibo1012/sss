77d4bc22481c87abb65b2cd5da3c6fdd73ec53e6
zhangyibo1012 <35024391+zhangyibo1012@users.noreply.github.com>
2018-09-20 22:22:36 +0800

    线程池

---
+++ src/main/java/com/zyblogs/concurrency/pattern/chapter01/SingletonEnum.java
@@ -0,0 +1,46 @@
+package com.zyblogs.concurrency.pattern.chapter01;
+
+import java.util.stream.IntStream;
+
+/**
+ * @Title: SingletonEnum.java
+ * @Package com.zyblogs.concurrency.pattern
+ * @Description:
+ * @Author ZhangYB
+ * @Version V1.0
+ */
+public class SingletonEnum {
+
+    private SingletonEnum(){ }
+
+    /**
+     *   枚举类型 构造函数私有,只会被装载一次。 线程安全的
+     */
+    private enum Singleton{
+        INSTANCE;
+
+        private final SingletonEnum instance;
+
+        Singleton(){
+            instance = new SingletonEnum();
+        }
+
+        public SingletonEnum getInstance(){
+            return instance;
+        }
+
+    }
+
+    public static SingletonEnum getInstance(){
+        return Singleton.INSTANCE.getInstance();
+    }
+
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1,100).forEach(i -> new Thread(String.valueOf(i)){
+            @Override
+            public void run() {
+                System.out.println(  SingletonEnum.getInstance());
+            }
+        }.start());
+    }
+}
